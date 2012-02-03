1825e983575f19e1022c602fa03b50c353fd13e2
Christopher J. Olido <colido@friendster.com>
2012-02-03 17:55:12 +0800

    adding dirs for demo_app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'c74473f32fb58805b54864c274f9a69d832310358f672b2e0f4dd8341972e721d181558cf6c6900323247c24ac679fc5ff077d3f6edcb836af35dda2ed96ba8e'
